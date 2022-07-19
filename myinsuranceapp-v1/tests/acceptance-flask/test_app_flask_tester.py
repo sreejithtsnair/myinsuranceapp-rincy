058a9897d5f5aab0a3624c899113fcc60854e008
Rincy Issac <rincy@allianz.com>
2022-07-19 12:06:13 +0200

    first commit

---
+++ myinsuranceapp-v1/tests/acceptance-flask/test_app_flask_tester.py
@@ -0,0 +1,12 @@
+import json
+import unittest
+from project import app
+
+
+class TestApp(unittest.TestCase):
+    token=''
+
+    def test_get_user_products(self):
+        return None
+
+    
---
+++ myinsuranceapp-v1/tests/acceptance-request/test_app_request.py
@@ -0,0 +1,9 @@
+import json
+import unittest
+import requests
+
+class TestApp(unittest.TestCase):
+    token=''
+    base_url='http://localhost:5000/api/v1'
+
+    
